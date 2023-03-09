4619b2861f6d43736e20225959b086e92b3269bf
Raimondo <SoyerTek@gmail.com>
2023-03-09 11:09:13 +0100

    cleaned and optimized code

--- cleaner.py
+++ cleaner.py
@@ -1,40 +1,30 @@
 import fitz
 import sys
 
-def contained(A, B) -> bool:
-    for x in B:
-        if x not in A:
-            return False
-    return True
-
-def uglyContains(A, B) -> bool:#B is contained in A?
+def uglyContains(A, B) -> bool:#is B contained in A?
     linesA = []
     linesB = []
-    for a in A:
-        linesA.append(a.strip().replace("\n", " ").split(" "))
+    #fuind all lines that aren't in A and B
     for b in B:
         if b in A:
             A.pop(A.index(b))
         else:
             linesB.append(b.strip().replace("\n", " ").split(" "))
+    for a in A:
+        linesA.append(a.strip().replace("\n", " ").split(" "))
 
-        for lineB in linesB:
-            possibleBlockA = [*range(len(linesA))]
-            for word in lineB:
-                for i in possibleBlockA:
-                    if word not in linesA[i]:
-                        possibleBlockA.pop(possibleBlockA.index(i))
+    for lineB in linesB:#if all words of a lineB are in a single lineA it can be removed
+        possibleBlockA = [*range(len(linesA))]
+        for word in lineB:
+            for i in possibleBlockA:
+                if word not in linesA[i]:
+                    possibleBlockA.pop(possibleBlockA.index(i))
 
-            if len(possibleBlockA) == 1:
-                linesB.remove(lineB)
+        if len(possibleBlockA) == 1:
+            linesB.remove(lineB)
+            linesA.pop(possibleBlockA[0])
 
     return len(linesB) == 0
-
-    
-
-def slimBlock(A) :
-    return [A[4].strip(), A[6]]
-
 
 args = sys.argv
 flag_r = False
@@ -46,7 +36,7 @@ if len(args) == 1:
 
 if len(args) > 0:
     flag_r = "-r" in args
-    flag_k = "-r" in args
+    flag_k = "-k" in args
 
 slides = fitz.open(args[1])
 clean = []
@@ -57,10 +47,7 @@ for i in range(len(slides)):
     blocks = []
     for block in slides[len(slides)-i-1].get_text("blocks"):
         blocks.append(block[4].strip())
-
-    
-
-
+        
     if not uglyContains(prev_blocks, blocks) : 
         #print(len(slides)-i-1)
         clean.append(len(slides)-i-1)
@@ -80,6 +67,7 @@ path = args[1].split("\\")[:-1]
 newfile = args[1].split("\\")[-1].split(".")[0] + "_clean.pdf"
 newfile = "\\".join(path) +"\\"+ newfile
 print("Saved in: " + newfile)
+print("Removed: " + str(len(dup)) + "/" + str(len(dup) + len(clean)))
 slides.save(newfile)
 slides.close()
 
